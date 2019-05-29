f1cca7a3b19fcc75fb915eae2deede7d25efe7cb
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2019-05-29 20:01:32 +0000

    practice w/ loops

--- script.js
+++ script.js
@@ -1,12 +1,74 @@
-const numbers = [67, 80, 65, 79, 86, 66, 75, 71, 81, 69, 90, 79, 68, 65, 73, 88];
 
-// 1. Convert numbers to Alphabet according to Ascii table using the fromCharCode method
-// Using a for loop:
-// Using the forEach method:
-// Using the map method:
+var numbers = [67, 80, 65, 79, 86, 66, 75, 71, 81, 69, 90, 79, 68, 65, 73, 88];
 
-// 2. Display numbers below showing only numbers that are greater than 72 and less then 88 (inclusive) using the filter method
-// Answer:
+//answer to number 1a
 
-// 3. Display the product of all numbers using reduce
-// Answer:
+function myFunction(){
+   
+    let result = [ ];
+    
+    for (var i = 0; i < numbers.length; i++)   {   //loop thru the array 'numbers'
+        
+        let convertedNumber = String.fromCharCode(numbers[i]);    //charCode method changes numbers into their respected letter character, one at a time
+        result.push(convertedNumber);    //push converted number 
+    }
+
+    document.getElementById("number1answer").innerHTML = result;  //display the converted numbers on the index.html file in a paragraph tag called 'number1answer'
+
+}
+
+
+//answer to number 1b
+
+function myFunction2(){
+
+numbers.forEach(numb => {
+  
+  const answer = String.fromCharCode(numb);
+  
+  document.getElementById("number2answer").innerHTML += answer;
+}
+)}
+
+
+// answer to number 1c
+
+function myFunction3(){
+
+var numbers = [67, 80, 65, 79, 86, 66, 75, 71, 81, 69, 90, 79, 68, 65, 73, 88];
+
+finalResult = numbers.map(function(value) {
+    return String.fromCharCode(value); })
+    
+    
+    document.getElementById("number3answer").innerHTML = finalResult
+
+}
+
+
+//answer to number 2
+
+function checkRange(numb) {
+  return numb > 72 ;
+//   return numb < 88;
+}
+
+function checkRange1(numb){
+    return numb < 88;
+}
+
+function myFunction4() {
+  document.getElementById("number4answer").innerHTML = numbers.filter(checkRange);
+}
+
+
+// answer to number 3
+
+function getProduct(product, num) {
+  return product * num;
+}
+
+function myFunction5(item) {
+  document.getElementById("number5answer").innerHTML = numbers.reduce(getProduct);
+}
+
